d03add8bd6b8e04af67679869cc79f9b3553e9bb
Scribani <scribani.andres@gmail.com>
2021-07-29 10:57:25 +0000

    Define vocab_word helper method and refactor code

--- handlers/handler.rb
+++ handlers/handler.rb
@@ -1,1 +1,18 @@
-# File for handler module
+require_relative "../helpers/presenter"
+
+module Handler
+  def vocab_word(input)
+    word = DictionaryService.words(@current_language, input)[0]
+    word_data = []
+    word_data << [word[:word], nil]
+
+    word[:meanings].each do |meaning|
+      word_data << ["uses", meaning[:partOfSpeech]]
+      word_data << ["definition", meaning[:definitions][0][:definition]]
+      word_data << ["example", meaning[:definitions][0][:example]]
+    end
+
+    print_definition(word_data)
+    @vocabulary_list << word_data
+  end
+end
