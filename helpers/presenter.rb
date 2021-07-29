ba246e2d00fa473408d21071616e1b9ac8b2c21f
Scribani <scribani.andres@gmail.com>
2021-07-29 11:00:32 +0000

    Define method to print definitions

--- helpers/presenter.rb
+++ helpers/presenter.rb
@@ -16,4 +16,19 @@ module Presenter
       "#" * 36
     ]
   end
+
+  def print_definition(word_data)
+    title = word_data.first.first
+
+    table = Terminal::Table.new do |t|
+      t.title = title.capitalize
+      t.add_row(word_data[1])
+      word_data[2..-1].each do |row|
+        t.add_separator if row[0] == "uses"
+        t.add_row(row)
+      end
+    end
+
+    puts table
+  end
 end
