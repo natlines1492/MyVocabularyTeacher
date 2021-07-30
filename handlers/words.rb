c2298929bbe59dcddea91072f6a38bf5e4f8f5db
Scribani <scribani.andres@gmail.com>
2021-07-30 08:35:26 +0000

    Modify methods to generate 10 questions at once

--- handlers/words.rb
+++ handlers/words.rb
@@ -33,12 +33,21 @@ module Handler
       word_rows
     end
 
-    def random_word_for_game
-      if @vocabulary_list.length <= 5
-        RandomWord.adjs.next.split("_")[0]
+    def random_words_for_game
+      vocabulary_sample = proc { random_words << @vocabulary_list.sample[:word] }
+      random_sample = proc { random_words << RandomWord.adjs.next.split("_")[0] }
+
+      random_words = []
+      words_in_vocabulary = @vocabulary_list.length
+      if words_in_vocabulary >= 5
+        5.times(&vocabulary_sample)
+        5.times(&random_sample)
       else
-        @vocabulary_list.sample[:word]
+        new_words_to_add = 10 - words_in_vocabulary
+        words_in_vocabulary.times(&vocabulary_sample)
+        new_words_to_add.times(&random_sample)
       end
+      random_words
     end
 
     def match_options(char_option, size, word_exclude)
@@ -53,22 +62,27 @@ module Handler
       word
     end
 
-    def generate_question(practice_type)
-      random_word = random_word_for_game
-      size = (random_word.size < 5 ? 5 : random_word.size)
-      available_definitions = vocabulary_word(random_word)[:definitions]
+    def generate_questions(practice_type)
+      random_words = random_words_for_game
+      questions = []
 
-      case practice_type
-      when "definitions"
-        question = available_definitions.sample[:definition]
-      when "examples"
-        question = available_definitions.sample[:example]
-        question = available_definitions.sample[:example] while question.nil?
+      random_words.each do |word|
+        size = (word.size < 5 ? 5 : word.size)
+        available_definitions = vocabulary_word(word)[:definitions]
+
+        case practice_type
+        when "definitions"
+          question = available_definitions.sample[:definition]
+        when "examples"
+          question = available_definitions.sample[:example]
+          question = available_definitions.sample[:example] while question.nil?
+        end
+        options = [word]
+        3.times { options << match_options(word[0..1], size, word) }
+
+        questions << { question: question, options: options, correct_answer: word }
       end
-      options = [random_word]
-      3.times { options << match_options(random_word[0..1], size, random_word) }
-
-      { question: question, options: options }
+      questions
     end
   end
 end
