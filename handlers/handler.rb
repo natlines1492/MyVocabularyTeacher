ebcde022d7ae312bd794a3462bda2e52332b846b
David Ortiz <david.ortiz.p@gmail.com>
2021-07-30 22:53:04 +0000

    save for add option in csv added

--- handlers/handler.rb
+++ handlers/handler.rb
@@ -15,4 +15,27 @@ module Handler
     print_definition(word_data)
     @vocabulary_list << word_data
   end
+
+  def save_vocabulary(vocabulary)
+    first_definitions = nth_group(vocabulary, 2)
+    Store.save(first_definitions, filename)
+  end
+
+  def nth_group(arr, num_elements)
+    new_arr = []
+    count = 0
+    condition = false
+    arr.each do |element|
+      if element[0] == "uses" # to be yield
+        count = 0
+        condition = true
+      end
+      next unless count <= num_elements && condition
+
+      new_arr << element
+      count += 1
+      condition = false if count == num_elements
+    end
+    new_arr
+  end
 end
--- services/store.rb
+++ services/store.rb
@@ -1,1 +1,17 @@
-# File for store module in services module
+require "csv"
+
+class Store
+  def self.save(vocabulary, _filename)
+    # create file if doesnt exist
+    data = vocabulary.map { |row| row.join(",") }.join("\n")
+    File.write("vocabulary.csv", data)
+  end
+
+  def self.save_csv(vocabulary, _filename)
+    CSV.open("vocabulary.csv", "w") do |csv|
+      vocabulary.each do |row|
+        csv << row
+      end
+    end
+  end
+end
