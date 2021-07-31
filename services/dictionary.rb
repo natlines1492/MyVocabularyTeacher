713985eb1a96ac3185a985cefc0e33e678ae8015
Scribani <scribani.andres@gmail.com>
2021-07-31 21:50:14 +0000

    Replace API with Web Scrapping methods

--- services/dictionary.rb
+++ services/dictionary.rb
@@ -1,13 +1,39 @@
-require "httparty"
-require "json"
+require "nokogiri"
+require "open-uri"
 
-class DictionaryService
-  include HTTParty
+module Scrapper
+  class Dictionary
+    def initialize
+      @base_uri = "https://www.wordreference.com/definition"
+    end
 
-  base_uri "https://api.dictionaryapi.dev/api/v2/entries"
+    def look_up(word)
+      doc = Nokogiri::HTML(URI.parse("#{@base_uri}/#{word}").open)
+      definition_section = doc.at_css("#otherDicts .entryRH")
+      return "No dictionary entry found for '#{word}'" unless definition_section
 
-  def self.words(language, word)
-    definitions = get("/#{language}/#{word}")
-    JSON.parse(definitions.body, symbolize_names: true) if definitions.body
+      definition_section_children = definition_section.children
+      uses = definition_section_children.select { |node| node.matches?(".rh_empos") }
+      description_element = definition_section_children.select { |node| node.name == "ol" }
+
+      word_data = { word: word, definitions: [] }
+      uses_count = uses.count
+      uses_count.times do |i|
+        word_data[:definitions].push(use_definition_example(uses[i], description_element[i]))
+      end
+
+      word_data
+    end
+
+    def use_definition_example(node_use, description_element)
+      use = node_use.content
+      node_definition = description_element.first_element_child.children.find { |node| node.matches?(".rh_def") }
+      definition = node_definition.children.find { |node| node.name == "text" }.content[1..-2]
+      node_example = node_definition.children.find { |node| node.matches?(".rh_ex") }
+      example_element = node_example.children.find { |node| node.name == "text" }
+      example = (example_element.nil? ? nil : example_element.content)
+
+      { uses: use, definition: definition, example: example }
+    end
   end
 end
