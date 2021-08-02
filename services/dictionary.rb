b80ef1c35d02fccc862986f4f1a84deab8666619
David Ortiz <david.ortiz.p@gmail.com>
2021-08-02 07:06:07 +0000

    workaround conditions in scrapping

--- services/dictionary.rb
+++ services/dictionary.rb
@@ -12,21 +12,30 @@ module Scrapper
       definition_section = doc.at_css("#otherDicts .entryRH")
       return puts "No dictionary entry found for '#{word}'" unless definition_section
 
-      description_element = definition_section.children.find do |node|
-        node.name == "ol" && node.children.text != "\r\n"
-      end
-
+      description_element = definition_section.at_css("li")
+      
       use_definition_example(word, description_element)
     end
 
     private
 
     def use_definition_example(word, description_element)
-      node_definition = description_element.first_element_child.children.find { |node| node.matches?(".rh_def") }
-      node_example = node_definition.children.find { |node| node.matches?(".rh_ex") }
-
-      definition = node_definition.children.find { |node| node.name == "text" }.content
-      example_element = node_example.children.find { |node| node.name == "text" }
+      node_definition = description_element.at_css(".rh_def")
+      node_example = node_definition.at_css(".rh_ex")
+      ul_condition = node_definition.at_css(".rh_sdef")
+      if ul_condition
+        definition = ul_condition.children.find { |node| node.name == "text" }.content
+      else
+        variable = node_definition.children.find { |node| node.name == "text" }
+        definition = variable.content if variable && variable.text != "\r\n"
+        unless variable && variable.text != "\r\n"
+          src_condition = node_definition.at_css(".rh_sc")
+          if src_condition
+            definition = src_condition.content
+          end
+        end
+      end
+      example_element = node_example.children.find { |node| node.name == "text" } unless node_example.nil?
       example = (example_element.nil? ? nil : example_element.content)
 
       { word: word, definition: definition, example: example }
