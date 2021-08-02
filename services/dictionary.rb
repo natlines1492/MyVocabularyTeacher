33e55cb513f5943e529a02e9d71b481e0ee5cf2b
David Ortiz <david.ortiz.p@gmail.com>
2021-08-02 07:06:14 +0000

    workaround conditions in scrapping

--- services/dictionary.rb
+++ services/dictionary.rb
@@ -13,32 +13,35 @@ module Scrapper
       return puts "No dictionary entry found for '#{word}'" unless definition_section
 
       description_element = definition_section.at_css("li")
-      
-      use_definition_example(word, description_element)
+
+      node_definition = description_element.at_css(".rh_def")
+      use_definition_example(word, node_definition)
     end
 
     private
 
-    def use_definition_example(word, description_element)
-      node_definition = description_element.at_css(".rh_def")
+    def use_definition_example(word, node_definition)
       node_example = node_definition.at_css(".rh_ex")
+      example_element = node_example.children.find { |node| node.name == "text" } unless node_example.nil?
+      example = (example_element.nil? ? nil : example_element.content)
+      definition = evaluate_definition(node_definition)
+      { word: word, definition: definition, example: example }
+    end
+
+    def evaluate_definition(node_definition)
       ul_condition = node_definition.at_css(".rh_sdef")
       if ul_condition
         definition = ul_condition.children.find { |node| node.name == "text" }.content
       else
         variable = node_definition.children.find { |node| node.name == "text" }
-        definition = variable.content if variable && variable.text != "\r\n"
-        unless variable && variable.text != "\r\n"
+        if variable && variable.text != "\r\n"
+          definition = variable.content
+        else
           src_condition = node_definition.at_css(".rh_sc")
-          if src_condition
-            definition = src_condition.content
-          end
+          definition = src_condition.content
         end
       end
-      example_element = node_example.children.find { |node| node.name == "text" } unless node_example.nil?
-      example = (example_element.nil? ? nil : example_element.content)
-
-      { word: word, definition: definition, example: example }
+      definition
     end
   end
 end
